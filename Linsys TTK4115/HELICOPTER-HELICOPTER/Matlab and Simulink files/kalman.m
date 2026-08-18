9f4a121994e7649c27b3adf1a911fc8cc510da19
fnilsen1 <filipok04@gmail.com>
2026-08-18 13:45:52 +0200

    Add Linsys helicopter lab files

---
+++ Linsys TTK4115/HELICOPTER-HELICOPTER/Matlab and Simulink files/kalman.m
@@ -0,0 +1,20 @@
+
+Joystick_gain_x = 1;
+Joystick_gain_y = -1;
+
+
+%%%%%%%%%%% Physical constants
+g = 9.81; % gravitational constant [m/s^2]
+l_c = 0.40; % distance elevation axis to counterweight [m]
+l_h = 0.66; % distance elevation axis to helicopter head [m]
+l_p = 0.175; % distance pitch axis to motor [m]
+m_c = 1.92; % Counterweight mass [kg]
+m_p = 0.65; % Motor mass [kg]
+
+V_s0 = 0;
+
+A_d = 0;
+B_d = 0;
+Q_d = 0;
+C_d = 0;
+R_d = 0;
